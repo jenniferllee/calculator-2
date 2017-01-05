1f8a88acd851b003dd64e67d2cc1cd09a529cebc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 17:45:20 -0800

    First working calculator.

--- calculator.py
+++ calculator.py
@@ -9,3 +9,37 @@ from arithmetic import *
 
 
 # Your code goes here
+
+# repeat forever:
+#     read input
+#     tokenize input
+#     if the first token is 'q'
+#         quit
+#     else
+#         decide which math function to call based on first token
+
+while True:
+    input_string = raw_input(">")
+    token = input_string.split(" ")
+
+    if token[0] == "q":
+        break
+    else:
+        if token[0] == "add":
+            print add(int(token[1]), int(token[2]))
+        elif token[0] == "subtract":
+            print subtract(int(token[1]), int(token[2]))
+        elif token[0] == "multiply":
+            print multiply(int(token[1]), int(token[2]))
+        elif token[0] == "divide":
+            print divide(int(token[1]), int(token[2]))
+        elif token[0] == "square":
+            print square(int(token[1]))
+        elif token[0] == "cube":
+            print cube(int(token[1]))
+        elif token[0] == "power":
+            print power(int(token[1]), int(token[2]))
+        elif token[0] == "mod":
+            print mod(int(token[1]), int(token[2]))
+        else:
+            print "Not a valid entry!"
